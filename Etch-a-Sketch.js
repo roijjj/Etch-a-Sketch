27f1aaf41057cd544747bd414d1e9036f0e78911
roijjj <eddyyozombie@gmail.com>
2025-06-23 16:51:47 -0600

    made the dive grid just need to finish css

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,13 @@
+const sect = document.querySelector(".container");
+let b
+for (let i = 0; i<16; i++){
+    const row = document.createElement("div")
+    sect.appendChild(row);``
+    for (let a = 0; a<16; a++){
+        const column = document.createElement("div");
+        row.appendChild(column);
+        column.textContent = "afffff"
+    }
+
+
+}
