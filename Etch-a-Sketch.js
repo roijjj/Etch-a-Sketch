24059592c8f23a92cdeb8536e3161e9a99220c1f
roijjj <eddyyozombie@gmail.com>
2025-06-24 14:39:30 -0600

    Add grid resize button and mouseover color effect
    Introduced a 'change button' to the UI and set up event listeners for mouseover coloring of grid cells. Added initial logic for resizing the grid, though the prompt functionality is not yet implemented.

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,16 +1,37 @@
 const sect = document.querySelector(".container");
-let b
-for (let i = 0; i<16; i++){
-    const row = document.createElement("div")
-    row.classList.add("block1")
-    sect.appendChild(row);
-    
-    for (let a = 0; a<16; a++){
-        const column = document.createElement("div");
-        column.classList.add("block2");
-        row.appendChild(column);
-        //column.textContent = "aff"
+let max = 16;
+
+
+function playgame(){
+    for (let i = 0; i<max; i++){
+        const row = document.createElement("div")
+        row.classList.add("block1")
+        sect.appendChild(row);
+        
+        for (let a = 0; a<max; a++){
+            const column = document.createElement("div");
+            column.classList.add("block2");
+            column.addEventListener("mouseover", function(){
+                this.style.backgroundColor = "blue";
+
+            });
+            row.appendChild(column);
+            
+            //column.textContent = "aff"
+        }
     }
 
+}
 
+playgame();
+
+
+let cbtn =  document.getElementById("#cbtn");
+cbtn.addEventListener('click', changesize());
+
+function changesize(){
+
+   return console.log("adfffff")
+   //return maxf = window.prompt("grid size","16");
+    
 }
